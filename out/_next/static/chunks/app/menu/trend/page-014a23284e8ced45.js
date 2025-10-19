f5703458d6d76c93ce03e481caa6f89cf6cdabca
numan <aydarnuman@icloud.com>
2025-10-19 07:56:00 +0300

    Fix GH Pages: replace 404.html with meta-refresh redirect, clean export and regenerate out/

--- out/_next/static/chunks/app/menu/trend/page-014a23284e8ced45.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[697,917,383,106,46,931,882,576,3,828,240],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
